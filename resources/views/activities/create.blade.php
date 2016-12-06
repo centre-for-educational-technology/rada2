7e5152c795bf1e4499c33f496619453789124839
Pjotr Savitski <gnum@tlu.ee>
2016-12-06 17:46:46 +0200

    Fixed activity create and edit pages.
    Added jQuery from CDN. This is needed until the script for the page is
    changed to use the Webpack approach.

--- resources/views/activities/create.blade.php
+++ resources/views/activities/create.blade.php
@@ -6,6 +6,10 @@
 @endsection
 
 @section('header-scripts')
+<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
 <script>
     window.onload = function() {
         $.getScript('https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.1.4/js/ion.rangeSlider.min.js')
