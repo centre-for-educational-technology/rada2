46ebef952d6d05b58e1df68abe7694a5c4c6c1d8
Pjotr Savitski <gnum@tlu.ee>
2017-01-16 16:23:02 +0200

    Changed activity_items table.
    Removed the "question" column (as storage logic for questions should
    will not use JSON-encoded strings) and added an "embedded_content"
    column (storing text value in separate table does not make any sense
    right now). Please run: php artisan migrate to apply database
    migrations.NB! Please note that additional module has been introduces,
    which means that you should also run "composer install" bofore going
    anything else.

---
+++ database/migrations/2017_01_16_140554_change_activity_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeActivityItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('activity_items', function (Blueprint $table) {
+            $table->dropColumn('question');
+            $table->text('embedded_content')->after('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('activity_items', function (Blueprint $table) {
+            $table->mediumText('question')->after('type');
+            $table->dropColumn('embedded_content');
+        });
+    }
+}
