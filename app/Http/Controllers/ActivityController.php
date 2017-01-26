3a5d2778cfba7711991fce04dc58494c561d52e9
Pjotr Savitski <gnum@tlu.ee>
2017-01-26 15:38:52 +0200

    Allowd play route for Activity to be public.

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -32,7 +32,7 @@ class ActivityController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth', ['except' => ['index', 'show']]);
+        $this->middleware('auth', ['except' => ['index', 'show', 'play']]);
     }
 
     /**
