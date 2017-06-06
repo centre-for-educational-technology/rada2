522d63df21b525c568b5c514976ded5cbc9a367b
Pjotr Savitski <gnum@tlu.ee>
2017-06-06 11:19:24 +0300

    Refernces #57, added Estonian translation base files.
    The files are just copies of the English ones. Still need translation.

---
+++ resources/lang/et/general.php
@@ -0,0 +1,123 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+   'activity-type' => [
+       'collecting-cards' => 'Collecting Cards',
+       'treasure-hunt' => 'Treasure Hunt',
+   ],
+   'activity-status' => [
+       'complete' => 'Complete Activity',
+       'ongoing' => 'Ongoing Activity',
+   ],
+   'language' => 'Language',
+   'languages' => [
+       'en' => 'English',
+       'et' => 'Estonian',
+       'ru' => 'Russian',
+       'fi' => 'Finnish',
+       'swe' => 'Swedish',
+   ],
+   'zoos' => [
+     'skansen' => 'Skansen',
+     'korkeasaari' => 'Korkeasaari',
+     'tallinn' => 'Tallinn Zoo',
+   ],
+   'question-types' => [
+     'one-correct-answer' => 'One correct answer',
+     'multiple-correct-answers' => 'Multiple correct answers',
+     'freeform-answer' => 'Freeform answer',
+     'match-pairs' => 'Match pairs',
+     'embedded-content' => 'Embedded content',
+     'photo' => 'Photo',
+     'information' => 'Information',
+   ],
+   'forms' => [
+       'labels' => [
+           'activity-type' => 'Activity Type',
+           'title' => 'Title',
+           'description' => 'Description',
+           'difficulty-level' => 'Difficulty Level',
+           'playing-time' => 'Playing Time',
+           'contact-information' => 'Contact Information',
+           'featured-image' => 'Freatured Image',
+           'zoo' => 'Zoo',
+           'question-type' => 'Question Type',
+           'location' => 'Location',
+           'activity-items' => 'Activity Items',
+           'proximity' => 'Proximity',
+           'search-text' => 'Search text',
+       ],
+       'help' => [
+           'playing-time' => 'Activity time in minutes',
+           'difficulty-level' => 'Choose a suitable range from available values. Defaults to all included.',
+           'featured-image' => 'An image in JPEG or PNG format. Uploaded image will be resized to 800 by 800 pixels.',
+           'map' => 'Please select the correct Zoo and drag the marker to the specific location.',
+           'question-types' => [
+               'information' => 'Description text will be displayed as an information for the location.',
+               'freeform-answer' => 'Input textarea will be displayed to the player.',
+               'embedded-content' => 'Embed code added into the textarea will be displayed to the player.',
+               'photo' => 'Player will be able to submit an existing photo or take one as an answer.',
+           ],
+       ],
+       'buttons' => [
+           'create' => 'Create',
+           'save' => 'Save',
+           'cancel' => 'Cancel',
+           'close' => 'Close',
+           'submit' => 'Submit',
+           'search' => 'Search',
+           'add-activity-items' => 'Add activity items',
+           'load-more' => 'Load more',
+           'add-option' => 'Add option',
+           'reset' => 'Reset',
+       ],
+       'placeholders' => [
+           'keyword-or-title' => 'Type a keyword or title',
+           'option-text' => 'Option text...',
+           'embedded-content' => 'Please add any embed codes here',
+           'search-text' => 'Enter a text fragment to search for',
+       ],
+       'options' => [
+           'any' => 'Any',
+       ]
+   ],
+   'actions' => [
+       'create' => 'Create',
+       'edit' => 'Edit',
+       'delete' => 'Delete',
+       'play' => 'Play',
+       'send-to-backpack' => 'Send To Backpack',
+       'download-baked-badge' => 'Download baked badge image',
+       'assertion-open' => 'Open badge assertion URL',
+   ],
+   'minutes' => 'minutes',
+   'confirmations' => [
+       'delete' => 'Are you sure you want to delete this item?',
+   ],
+   'date-time' => [
+       'formats' => [
+           'short' => 'F d, Y',
+           'medium' => 'F d, Y H:i',
+           'long' => 'F d, Y, H:i:s',
+       ],
+       'created-at' => 'Created at',
+       'updated-at' => 'Updated at',
+   ],
+   'roles' => [
+       'admin' => 'Administrator',
+       'zooAdmin' => 'Administrator',
+       'zooMember' => 'Member',
+   ],
+];
---
+++ resources/lang/et/pages.php
@@ -0,0 +1,122 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pages Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used on various pages.
+    |
+    */
+
+    'welcome' => [
+        'jumbotron' => [
+            'heading' => 'What is SmartZoos?',
+            'content' => 'SmartZoos integrates the zoos of the CB region to form a joint tourist attraction through developing, implementing and joint marketing of a cross-border service package of creative adventure learning with mobile devices.',
+        ],
+    ],
+    'activities' => [
+        'index' => [
+            'heading' => 'Activities',
+            'none-found' => 'No Activities could be found.',
+        ],
+        'create-or-edit' => [
+            'keywords' => 'Keywords',
+            'items-found' => 'items found',
+            'tooltips' => [
+                'proximity-check' => 'Determines if proximity check is applied',
+            ],
+            'help' => [
+                'proximity' => 'Only set proximity radius in meters if defalult value of :default is not suitable. We recommend setting it to a value between 25 to 100 meters. If you uncheck the checkbox situated to the right of the field, then the game will not check proximity at all.',
+            ],
+        ],
+    ],
+    'activity-items' => [
+        'index' => [
+          'heading' => 'Activity Items',
+          'none-found' => 'No Activity Items could be found.',
+        ],
+        'create-or-edit' => [
+            'set-current-position' => 'Set current position',
+            'geolocation-error' => 'Error: Geolocation could not be determined',
+        ],
+    ],
+    'manage' => [
+        'users' => [
+            'index' => [
+                'heading' => 'Manage Users',
+                'none-found' => 'No User could be found',
+                'accounts' => 'Accounts',
+                'roles' => 'Roles',
+                'manage-roles' => 'Manage roles',
+                'remove-role' => 'Remove role',
+                'confirmations' => [
+                    'role' => 'Are you sure you want to remove selected role?',
+                ],
+                'modal' => [
+                    'title' => 'Manage user roles',
+                ],
+            ]
+        ],
+    ],
+    'play' => [
+        'game' => [
+            'loading' => 'Game Loading ...',
+            'its-you' => 'It\'s you',
+            'info' => 'Game information',
+            'position-tracking' => 'Enable or disable position tracking',
+            'exit' => 'Exit current game',
+            'exit-confirmation' => 'Are you sure you want to exit the game? Only authenticated users would be able to continue later.',
+            'textual-answer-placeholder' => 'Answer text...',
+            'image-format-hint' => 'Please use only PNG or JPG/JPEG images.',
+            'apply-item-bounds' => 'Show all available Activity Item points on the map',
+            'results-heading' => 'These are your results',
+            'number-of-questions' => 'No. of Questions',
+            'gps-error' => 'GPS signal not found.',
+            'go' => 'GO!',
+            'icons' => [
+                'help' => 'Each icon has a colour code that indicates the state of a question.',
+                'active' => 'Unanswered',
+                'inactive' => 'Out of reach',
+                'correct' => 'Correct',
+                'incorrect' => 'Incorrect answer',
+            ],
+        ],
+    ],
+    'dashboard' => [
+        'title' => 'Dashboard',
+        'heading' => 'Welcome, :name!',
+        'none-found' => 'No Games or Activities found',
+        'games-and-activities' => 'Games & activities',
+        'finished' => 'Finished :date',
+        'started' => 'Started :date',
+        'btn' => [
+            'view-results' => 'View results',
+            'continue' => 'Continue',
+        ],
+    ],
+    'activity-results-index' => [
+        'title' => 'Activities',
+        'heading' => 'Results of Activities',
+        'zoos' => 'Displayed activities belong to these Zoos',
+    ],
+    'activity-results' => [
+        'heading' => 'Results of :title',
+
+    ],
+    'profile' => [
+        'title' => 'Profile',
+        'heading' => 'Profile of :name',
+        'labels' => [
+            'badges-earned' => 'Badges Earned',
+        ],
+    ],
+    'profile-edit' => [
+        'help' => [
+            'password' => 'Please leave password field empty, unless you want to have it changed.',
+        ]
+    ],
+
+];
