e3c52dd0d5c8df18b6d77b685dc8ecdca6f9f8f5
Pjotr Savitski <gnum@tlu.ee>
2018-04-17 11:23:26 +0300

    References #198, defined scheduled task.
    The task would be run on daily basis and delete any unverified user
    accounts that have been registered more than 48 hours ago.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\User;
+use Carbon\Carbon;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,8 +26,16 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->call(function()
+        {
+            User::where('verified', 0)->where('created_at', '<', Carbon::now()->subHours(48))->chunk(50, function($users)
+            {
+                foreach ( $users as $user )
+                {
+                    $user->delete();
+                }
+            });
+        })->daily()->name('removeUnverifiedUsers')->withoutOverlapping();
     }
 
     /**
