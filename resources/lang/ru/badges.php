88d170c4011e08d6859137929f4dadc602ee9572
tallinnzoo <33761079+tallinnzoo@users.noreply.github.com>
2018-05-07 15:42:50 +0300

    Update badges.php

--- resources/lang/ru/badges.php
+++ resources/lang/ru/badges.php
@@ -13,43 +13,43 @@ return [
 
     'novice_gamer' => [
         'name' => 'Начинающий игрок',
-        'description' => 'Для завершения первой тропы.',
+        'description' => 'За прохождение первой тропы.',
     ],
     'pro_gamer' => [
         'name' => 'Профессиональный игрок',
-        'description' => 'Become a true game master by completing twenty-five Activities.',
+        'description' => 'Станьте настоящим мастером игры, пройдя двадцать пять троп.',
     ],
     'seasoned_creator' => [
-        'name' => 'Seasoned Creator',
-        'description' => 'For creating five Activities.',
+        'name' => 'Опытный Создатель',
+        'description' => 'Награда за создание пяти троп.',
     ],
     'novice_creator' => [
-        'name' => 'Novice Creator',
-        'description' => 'For creating first Activity.',
+        'name' => 'Начинающий Создатель',
+        'description' => 'Награда за создание первой тропы.',
     ],
     'seasoned_gamer' => [
-        'name' => 'Seasoned Gamer',
-        'description' => 'For completing five Activities.',
+        'name' => 'Опытный игрок',
+        'description' => 'Награда за прохождение пяти троп.',
     ],
     'pro_creator' => [
-        'name' => 'Professional Creator',
-        'description' => 'Master the art of creation by bringing twenty-five Activities into existence.',
+        'name' => 'Профессиональный Создатель.',
+        'description' => 'Освойте искусство творения, создав двадцать пять троп.',
     ],
     'veteran_creator' => [
-        'name' => 'Veteran Creator',
-        'description' => 'For creating ten Activities.',
+        'name' => 'Бывалый Создатель',
+        'description' => 'Награда за создание десяти троп.',
     ],
     'veteran_gamer' => [
-        'name' => 'Veteran Gamer',
-        'description' => 'For completing ten Activities.',
+        'name' => 'Бывалый игрок',
+        'description' => 'Награда за прохождение десяти троп.',
     ],
     'social' => [
-        'name' => 'Socializer',
-        'description' => 'For logging in with social network.',
+        'name' => 'Мастер общения',
+        'description' => 'Награда за вход через социальную сеть.',
     ],
     'register' => [
-        'name' => 'Accountant',
-        'description' => 'For creating an account.',
+        'name' => 'Создатель аккаунта',
+        'description' => 'Награда за создание аккаунта.',
     ],
 
 ];
