149d6b624ec5f8b5f7fd1ddc60013cf6ee50fe3c
siim <siim@dolmit.com>
2019-09-03 22:14:19 +0300

    Rada-61 Manually grading "missing word" question

--- resources/assets/js/components/Grading/QuestionTypes/MissingWord.vue
+++ resources/assets/js/components/Grading/QuestionTypes/MissingWord.vue
@@ -3,9 +3,7 @@
         <br />
         <p>{{ answer.missing_word }}</p>
         <h4>{{ $t('pages.grading.index.heading-answer')}}</h4>
-        <p>
-            {{ answerText }}
-        </p>
+        <p v-html="answerText"></p>
         <single-grading
                 :maxGrade="maxPoints"
                 :answerId="answer.id"
@@ -21,16 +19,14 @@
         },
         data() {
             return {
-                inAjaxCall: false
+                inAjaxCall: false,
+                questionMissingWords: this.getMissingWords(this.answer.missing_word),
+                answerMissingWords: this.getMissingWords(this.getAnswerText())
             }
         },
         computed: {
             answerText() {
-                const answer = JSON.parse(this.answer.answer);
-                if (answer && typeof answer.text !== 'undefined') {
-                    return answer.text;
-                }
-                return '-';
+                return this.replaceAnswerText();
             },
             maxPoints() {
                 return parseInt(this.answer.max_points);
@@ -48,6 +44,52 @@
             },
             markGraded(answerId, grade) {
                 this.$parent.markGraded(answerId, grade);
+            },
+            replaceAnswerText() {
+                let answerText = this.getAnswerText();
+                let questionMissingWordsLength = this.questionMissingWords.length;
+
+                for (let i=0; i<questionMissingWordsLength; i++) {
+                    let originalWord = this.questionMissingWords[i];
+                    let answerWord = this.answerMissingWords[i];
+                    let search = '{' + answerWord.word + '}';
+                    let replacer = '';
+                    if (answerWord.word !== originalWord.word) {
+                        replacer = '{<span style="color: red; font-weight: bold;">' + answerWord.word + '</span> | ';
+                        replacer += '<span style="color: green; font-weight: bold;">' + originalWord.word + '</span>}';
+                    } else {
+                        replacer = '{<span style="color: green; font-weight: bold;">' + originalWord.word + '</span>}';
+                    }
+
+                    answerText = answerText.replace(search, replacer);
+                }
+
+                return answerText;
+            },
+            getAnswerText() {
+                const answer = JSON.parse(this.answer.answer);
+                if (answer && typeof answer.text !== 'undefined') {
+                    return answer.text;
+                }
+                return '-';
+            },
+            getMissingWords(str) {
+                const regex = RegExp('\{[A-Za-z0-9\.\s-]+\}','g');
+                let missingWords = [];
+                let array;
+                let index = 0;
+
+                while ( (array = regex.exec(str)) !== null) {
+                    let missingWord = array[0].substring(1).slice(0, -1);
+                    missingWords[index] = {
+                        word: missingWord,
+                        start: array.index,
+                        end: regex.lastIndex
+                    };
+                    index ++;
+                }
+
+                return missingWords;
             }
         }
     }
