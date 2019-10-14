db7bacf17c5a416acb4f3952ac77618da60bc859
siim <siim.liimand@gmail.com>
2019-10-14 18:16:15 +0300

    Rada-107 Player summary segment

--- app/Http/Controllers/GameStatisticsController.php
+++ app/Http/Controllers/GameStatisticsController.php
@@ -12,13 +12,21 @@ use Illuminate\Support\Facades\DB;
 use PhpOffice\PhpSpreadsheet\Exception;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class GameStatisticsController extends Controller
 {
 
-    public function index(Game $game)
+    public const P_ORDER_BY_TYPE_POINTS = 1;
+    public const P_ORDER_BY_TYPE_NAME = 2;
+    public const P_ORDER_BY_TYPE_TIME = 3;
+    public const P_ORDER_BY_TYPE_ANSWERED = 4;
+    public const P_ORDER_BY_TYPE_GRADED = 5;
+    public const P_ORDER_BY_TYPE_NOT_GRADED = 6;
+
+    public function index(Request $request, Game $game)
     {
         /** @var Activity $activity */
         $activity = $game->activity;
@@ -32,11 +40,73 @@ class GameStatisticsController extends Controller
             $points = is_array($data) ? array_sum($data) : $exercise->points;
             $totalPoints += $points;
         }
+
+        $playersSortOrder = static::P_ORDER_BY_TYPE_POINTS;
+        if ($request->get('playersSortOrder')) {
+            $playersSortOrder = $request->get('playersSortOrder');
+        }
+        $playersSortOrderDir = 'DESC';
+        if ($request->get('playersSortOrderDir')) {
+            $playersSortOrderDir = $request->get('playersSortOrderDir');
+        }
+        $players = $this->getPlayers($game, $playersSortOrder, $playersSortOrderDir);
+
         return view('manage.game-statistics')->with([
             'game' => $game,
             'exercises' => $exercises,
             'countExercises' => $exercisesQuery->count(),
-            'totalPoints' => $totalPoints
+            'totalPoints' => $totalPoints,
+            'players' => $players,
+            'playersSortOrder' => $playersSortOrder,
+            'playersSortOrderDir' => $playersSortOrderDir
+        ]);
+    }
+
+    public function getPlayers(Game $game, int $orderByType, string $playersSortOrderDir): array
+    {
+        switch($orderByType) {
+            case static::P_ORDER_BY_TYPE_POINTS:
+                $orderBy = '`points` ' . $playersSortOrderDir;
+                break;
+            case static::P_ORDER_BY_TYPE_NAME:
+                $orderBy = '`name` ' . $playersSortOrderDir;
+                break;
+            case static::P_ORDER_BY_TYPE_TIME:
+                $orderBy = '`time` ' . $playersSortOrderDir;
+                break;
+            case static::P_ORDER_BY_TYPE_ANSWERED:
+                $orderBy = '`count_answers` ' . $playersSortOrderDir;
+                break;
+            case static::P_ORDER_BY_TYPE_GRADED:
+                $orderBy = '`graded` ' . $playersSortOrderDir;
+                break;
+            case static::P_ORDER_BY_TYPE_NOT_GRADED:
+                $orderBy = '`not_graded` ' . $playersSortOrderDir;
+                break;
+            default:
+                $orderBy = '`points` ' . $playersSortOrderDir;
+        }
+        $sql = '
+        SELECT IF( `u`.`id` IS NOT NULL, `u`.`name`, `g`.`nickname` ) AS user_name,
+               SUM(`ga`.`grade`) AS points,
+               TIMEDIFF(`pp2`.`created_at`, `pp1`.`created_at`) AS time,
+               (SELECT COUNT(`ga1`.`id`) FROM `game_answers` AS `ga1` WHERE `ga1`.`game_id` = `g`.`id`) AS count_answers,
+               (SELECT COUNT(`aa1`.`activity_item_id`) FROM `activity_activity_item` AS `aa1` WHERE `aa1`.`activity_id` = `g`.`activity_id`) AS count_questions,
+               (SELECT COUNT(`ga2`.`id`) FROM `game_answers` AS `ga2` WHERE `ga2`.`game_id` = `g`.`id` AND `ga2`.`grade` IS NOT NULL) AS graded,
+               (SELECT COUNT(`ga2`.`id`) FROM `game_answers` AS `ga2` WHERE `ga2`.`game_id` = `g`.`id` AND `ga2`.`grade` IS NULL) AS not_graded
+          FROM `games` AS `g`
+          LEFT JOIN `users` AS `u` ON `g`.`user_id` = `u`.`id`
+          LEFT JOIN `game_answers` AS `ga` ON `ga`.`game_id` = `g`.`id`
+          LEFT JOIN `player_positions` AS `pp1` ON `pp1`.`game_id` = `g`.`id`
+          LEFT JOIN `player_positions` AS `pp2` ON `pp2`.`game_id` = `g`.`id`
+         WHERE `g`.`activity_id` = :activity_id
+           AND `pp1`.`created_at` = (SELECT MIN(`pp11`.`created_at`) FROM `player_positions` AS `pp11` WHERE `pp11`.`game_id` = `g`.`id`)
+           AND `pp2`.`created_at` = (SELECT MAX(`pp22`.`created_at`) FROM `player_positions` AS `pp22` WHERE `pp22`.`game_id` = `g`.`id`)
+         GROUP BY `g`.`id`
+         ORDER BY '.$orderBy.'
+        ';
+        return DB::select($sql, [
+            'activity_id' => $game->activity_id
         ]);
     }
 
